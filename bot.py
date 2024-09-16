bf42f1b486d86bff7d5c0d0b8ae3662b8c21c6ba
xumpocmb <x.xumpocmb@gmail.com>
2024-09-16 08:02:44 +0300

    bot 0.9.5

--- bot.py
+++ bot.py
@@ -3,18 +3,19 @@ import pandas as pd
 import json
 import os
 import threading
-from tkinter import Tk, Label, Entry, Button, Checkbutton, IntVar, filedialog
+from tkinter import Tk, Label, Entry, Button, Checkbutton, IntVar, filedialog, messagebox
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 CREDENTIALS_FILE = 'credentials.json'
 
 
 def load_credentials():
+    """Загружает учетные данные из файла JSON."""
     if os.path.exists(CREDENTIALS_FILE):
         with open(CREDENTIALS_FILE, 'r') as file:
             data = json.load(file)
@@ -24,6 +25,7 @@ def load_credentials():
 
 
 def save_credentials():
+    """Сохраняет учетные данные в файл JSON."""
     if remember_var.get() == 1:
         credentials = {
             "login": login_entry.get(),
@@ -38,116 +40,121 @@ def save_credentials():
 
 
 def select_file():
+    """Открывает диалог выбора файла и вставляет его путь в текстовое поле."""
     file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+    file_entry.delete(0, 'end')
     file_entry.insert(0, file_path)
 
 
 def start_processing_thread():
+    """Запускает процесс обработки данных в отдельном потоке."""
     threading.Thread(target=start_processing).start()
 
 
+def init_driver():
+    """Инициализирует и возвращает объект Selenium WebDriver."""
+    service = Service('chromedriver-win64/chromedriver.exe')
+    options = webdriver.ChromeOptions()
+    return webdriver.Chrome(service=service, options=options)
+
+
+def login_to_site(driver, login, password):
+    """Выполняет вход на сайт с указанными логином и паролем."""
+    try:
+        driver.get('https://kiber-one.club/')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'login')))
+        driver.find_element(By.NAME, 'login').send_keys(login)
+        driver.find_element(By.NAME, 'password').send_keys(password)
+        driver.find_element(By.XPATH, '//*[@id="loginForm"]/table/tbody/tr[4]/td/input').click()
+        WebDriverWait(driver, 10).until(EC.url_changes('https://kiber-one.club/'))
+    except (NoSuchElementException, TimeoutException) as e:
+        messagebox.showerror("Ошибка входа", f"Не удалось войти на сайт: {e}")
+        driver.quit()
+        return False
+    return True
+
+
+def process_user(driver, row):
+    """Обрабатывает каждого пользователя в таблице."""
+    try:
+        search_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div[2]/div/div/div[2]/input')
+        search_field.clear()
+        search_field.send_keys(row['фио'])
+        time.sleep(3)
+
+        user_item = driver.find_element(By.XPATH, '//div[contains(@class, "user_item") and @style="display: table-row;"]')
+        user_item.find_element(By.TAG_NAME, 'a').click()
+
+        time.sleep(2)
+        iter_count = int(row['кибероны']) // 5
+
+        for _ in range(iter_count):
+            button_change_kiberons = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[2]/div[2]/div/div/div[1]/div[1]/span/span')
+            button_change_kiberons.click()
+
+            select1 = Select(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "fc_field_sign_id"))))
+            select1.select_by_visible_text("Начисление")
+
+            select2 = Select(WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "fc_field_cause_id"))))
+            select2.select_by_index(4)
+
+            save_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "sendsave")))
+            save_button.click()
+
+            close_modal_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "uss_modal_close")))
+            close_modal_element.click()
+
+            time.sleep(1)
+
+        row["кибероны"] = None
+        driver.back()
+        time.sleep(3)
+        driver.refresh()
+
+    except (NoSuchElementException, TimeoutException):
+        print(f"Пользователь {row['фио']} не найден или произошла ошибка. Пропускаю.")
+        return False
+
+    return True
+
+
 def start_processing():
+    """Основная логика обработки данных."""
     save_credentials()
     file_path = file_entry.get()
     if not file_path:
-        return None
+        messagebox.showerror("Ошибка", "Выберите файл.")
+        return
+
     login = login_entry.get()
     password = password_entry.get()
 
     df = pd.read_excel(file_path)
+    driver = init_driver()
 
-    service = Service('chromedriver-win64/chromedriver.exe')
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
+    if not login_to_site(driver, login, password):
+        return
 
-    driver.get('https://kiber-one.club/')
-    time.sleep(2)
+    try:
+        driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/a').click()
+        time.sleep(2)
 
-    username_field = driver.find_element(By.NAME, 'login')
-    password_field = driver.find_element(By.NAME, 'password')
+        for index, row in df.iterrows():
+            if pd.notna(row["фио"]) and pd.notna(row["кибероны"]):
+                if process_user(driver, row):
+                    df.at[index, "кибероны"] = None
 
-    username_field.send_keys(login)
-    password_field.send_keys(password)
-
-    login_button = driver.find_element(By.XPATH, '//*[@id="loginForm"]/table/tbody/tr[4]/td/input')
-    login_button.click()
-
-    time.sleep(2)
-
-    users_tab = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/a')
-    users_tab.click()
-
-    time.sleep(2)
-
-    for index, row in df.iterrows():
-        if pd.notna(row["фио"]) and pd.notna(row["кибероны"]):
-            search_field = driver.find_element(By.XPATH,
-                                               '/html/body/div[1]/div/div/div/div/div[2]/div[2]/div/div/div[2]/input')
-            search_field.clear()
-            search_field.send_keys(row['фио'])
-
-            time.sleep(3)
-
-            try:
-                print(f'Processing row {index}: ФИО: {row["фио"]} | Кибероны: {row["кибероны"]}')
-                user_item = driver.find_element(By.XPATH,
-                                                '//div[contains(@class, "user_item") and @style="display: table-row;"]')
-
-                user_link = user_item.find_element(By.TAG_NAME, 'a')
-
-                user_link.click()
-
-                time.sleep(3)
-
-                iter_count = int(row['кибероны']) // 5
-
-                for _ in range(iter_count):
-                    button_change_kiberons = driver.find_element(By.XPATH,
-                                                                 '/html/body/div[1]/div/div/div/div/div[2]/div[2]/div/div/div[1]/div[1]/span/span')
-                    button_change_kiberons.click()
-
-                    select_element1 = WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.ID, "fc_field_sign_id")))
-                    select1 = Select(select_element1)
-                    select1.select_by_visible_text("Начисление")
-
-                    time.sleep(1)
-
-                    select_element2 = WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.ID, "fc_field_cause_id")))
-                    select2 = Select(select_element2)
-                    select2.select_by_index(4)
-
-                    time.sleep(1)
-
-                    save_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "sendsave")))
-                    save_button.click()
-
-                    time.sleep(1)
-
-                    close_modal_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "uss_modal_close")))
-                    close_modal_element.click()
-
-                    time.sleep(1)
-
-
-                df.at[index, "кибероны"] = None
-
-                driver.back()
-                time.sleep(3)
-                driver.refresh()
-                time.sleep(3)
-
-            except:
-                print(f"Пользователь {row['фио']} не найден. Пропускаю.")
-                continue
-    df.to_excel(file_path, index=False)
-    driver.quit()
+        df.to_excel(file_path, index=False)
+        messagebox.showinfo("Завершено", "Обработка завершена успешно.")
+    except Exception as e:
+        messagebox.showerror("Ошибка", f"Произошла ошибка во время обработки: {e}")
+    finally:
+        driver.quit()
 
 
 if __name__ == "__main__":
     root = Tk()
-    root.title("Скрипт для обработки Киберонов")
+    root.title("KIBER Club - Бот для начисления Киберонов")
 
     file_label = Label(root, text="Выберите файл с данными:")
     file_label.grid(row=0, column=0)
